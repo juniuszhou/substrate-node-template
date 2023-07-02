ca8d8a34a85db70f53c00492b07d56f749b4f48e
junius zhou <junius.zhou@gmail.com>
2023-07-02 21:18:01 +0800

    submit ok

--- node/src/extra_thread.rs
+++ node/src/extra_thread.rs
@@ -5,6 +5,11 @@ use sp_blockchain::HeaderMetadata;
 use tokio::time::Duration;
 // use sc_transaction_pool_api::TransactionPool;
 use std::sync::Arc;
+use sp_core::Bytes;
+use codec::{Decode, Encode};
+
+use sc_transaction_pool_api::
+	TransactionSource;
 
 pub struct ExtraThread<TransactionPool>
 where
@@ -18,14 +23,17 @@ where
 	TransactionPool: sc_transaction_pool_api::TransactionPool + 'static,
 {
 	// submit an extrinsic
+    let ext: Bytes = Bytes::from(vec![0;10]);
 
-	// config.transaction_pool.submit_one(
+	let xt = match Decode::decode(&mut &ext[..]) {
+        Ok(xt) => xt,
+        Err(err) => return,
+    };
+    // let best_block_hash = self.client.info().best_hash;
+    config.transaction_pool
+        .submit_one(&sp_runtime::generic::BlockId::Number(0_u32.into()), TransactionSource::Local, xt)
+        .await;
 		
-	// 	Default::default(),
-	// 	Default::default(),
-    //     Default::default(),
-	// );
-
 	loop {
 		tokio::time::sleep(Duration::from_secs(10)).await;
 		log::info!("ExtraThread::run");
